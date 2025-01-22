4763e06353db7d7ff493558baf9468ffe7f21fdb
Baptiste Lyet <44911483+Babali42@users.noreply.github.com>
2025-01-22 20:10:17 +0100

    update angular 18 wip

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
-import {HttpClientModule} from '@angular/common/http';
+import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
 import {InMemoryDataService} from "./adapters/secondary/in-memory-data.service";
 import {SequencerComponent} from "./components/sequencer/sequencer.component";
@@ -9,21 +9,21 @@ import {GenresAdapterService} from "./adapters/secondary/genres-adapter.service"
 import {LoadingBarModule} from '@ngx-loading-bar/core';
 import {HTTP_INTERCEPTORS} from '@angular/common/http';
 import {LoadingInterceptor} from './interceptors/loading.interceptor';
-import {environment} from "../environments/environment";
 import {provideRouter, RouterOutlet, Routes} from "@angular/router";
 import {BeatCreatorComponent} from "./components/beat-creator/beat-creator.component";
-import { FormsModule } from '@angular/forms';
+import {FormsModule} from '@angular/forms';
 
 export const routes: Routes = [
-  { path: '', component: SequencerComponent },
-  { path: 'add-beat', component: BeatCreatorComponent }
+  {path: '', component: SequencerComponent},
+  {path: 'add-beat', component: BeatCreatorComponent}
 ];
 import {IManageGenresToken} from "./domain/ports/secondary/i-manage-genres";
+import {environment} from "../environments/environment";
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    HttpClientModule,
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+  imports: [BrowserModule,
     LoadingBarModule,
     FormsModule,
     // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
@@ -34,15 +34,20 @@ import {IManageGenresToken} from "./domain/ports/secondary/i-manage-genres";
     ) : [],
     RouterOutlet
   ],
-  declarations: [AppComponent],
   providers: [
-    // Inject adapters into domain classes
     {provide: IManageGenresToken, useClass: GenresAdapterService},
     {provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true},
-    provideRouter(routes)
-  ],
-  bootstrap: [AppComponent]
+    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()),
+    { provide: IManageGenresToken, useClass: GenresAdapterService },
+    //@ts-ignore
+    environment.httpClientInMemory ? HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+      dataEncapsulation: false
+    }).providers : []
+  ]
 })
+
+
 export class AppModule {
 }
 
