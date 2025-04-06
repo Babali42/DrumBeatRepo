2dafecb2d6e469a6aa7ffd4e8e331510f51af693
Baptiste Lyet <44911483+Babali42@users.noreply.github.com>
2025-04-06 09:03:56 +0200

    Bugfix (in memory beat gateway) : error between rxjs and promise

--- frontend/src/app/adapters/secondary/in-memory-beat-gateway.ts
+++ frontend/src/app/adapters/secondary/in-memory-beat-gateway.ts
@@ -1,9 +1,10 @@
-import { BeatsGroupedByGenre } from "src/app/domain/beatsGroupedByGenre";
+import {BeatsGroupedByGenre} from "src/app/domain/beatsGroupedByGenre";
 import IManageBeats from "../../domain/ports/secondary/i-manage-beats";
 import {beatsGroupedByGenre} from "./beats/beats-in-memory-data";
+import {firstValueFrom, of} from "rxjs";
 
 export class InMemoryBeatGateway implements IManageBeats {
-    getBeatsGroupedByGenres(): Promise<BeatsGroupedByGenre[]> {
-        return new Promise(() => beatsGroupedByGenre);
-    }
+  getBeatsGroupedByGenres(): Promise<BeatsGroupedByGenre[]> {
+    return firstValueFrom(of(beatsGroupedByGenre));
+  }
 }
