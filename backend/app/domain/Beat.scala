6bad3cd257b35060c1b6acc1a23fd27c6938fde6
Baptiste <bab07ali@gmail.com>
2025-06-26 18:14:39 +0200

    Backend : Chore : Delete backend folder

--- backend/app/domain/Beat.scala
+++
@@ -1,9 +0,0 @@
-package domain
-
-case class Beat(id: String, label: String, bpm: Int, genre: String, tracks: Seq[Track]) {
-  def as[T](implicit f: Beat => T): T = f(this)
-}
-
-object Beat {
-  implicit def beatMapper: Beat => primary.Beat = (beat: Beat) => primary.Beat(beat.id, beat.label, beat.bpm, beat.genre, beat.tracks.map(x => x.as[primary.Track]))
-}
