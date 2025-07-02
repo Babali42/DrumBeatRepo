37ac1ee9a38a5f6f11bb02e2976e13d3d5c221d1
Baptiste Lyet <44911483+Babali42@users.noreply.github.com>
2025-07-02 20:16:39 +0200

    Chore : Update angular 18 to 20

--- frontend/src/app/services/light-dark-mode/mode-toggle.service.ts
+++ frontend/src/app/services/light-dark-mode/mode-toggle.service.ts
@@ -1,5 +1,4 @@
-import {DOCUMENT} from "@angular/common";
-import {Inject, Injectable} from "@angular/core";
+import {Inject, Injectable, DOCUMENT} from "@angular/core";
 import {BehaviorSubject, Observable} from "rxjs";
 import {Mode} from "./mode-toggle.model";
 
