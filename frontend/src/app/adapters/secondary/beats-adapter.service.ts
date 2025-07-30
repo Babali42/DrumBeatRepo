ef12fb5c40115b2832887b83c7a395abda29b3eb
Baptiste <bab07ali@gmail.com>
2025-07-30 15:25:29 +0200

    Refacto : Remove beats adapter service

--- frontend/src/app/adapters/secondary/beats-adapter.service.ts
+++ frontend/src/app/adapters/secondary/beats-adapter.service.ts
@@ -1,49 +1,13 @@
 import {Injectable} from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import IManageBeats from "../../domain/ports/secondary/i-manage-beats";
 import {BeatsGroupedByGenre} from "../../domain/beatsGroupedByGenre";
-import {Effect} from 'effect';
-import {lastValueFrom} from "rxjs";
-import {CompactBeat} from "../../domain/compact-beat";
+import {beatsGroupedByGenre} from "./beats/beats-in-memory-data";
+import {firstValueFrom, of} from "rxjs";
 
 @Injectable({providedIn: 'root'})
 export class BeatsAdapterService implements IManageBeats {
-
-  private readonly beatsUrl = 'beats/';
-
-  constructor(private readonly http: HttpClient) {
-
-  }
-
   getBeatsGroupedByGenres = (): Promise<readonly BeatsGroupedByGenre[]> => {
-    const effect = Effect.tryPromise({
-      try: async () => {
-        const beats = await lastValueFrom(this.http.get<CompactBeat[]>(this.beatsUrl));
-        const beatsGroupedByGenres = this.groupBy(beats, (beat) => beat.genre);
-        return Object.keys(beatsGroupedByGenres).map((genre) => ({
-          label: genre,
-          beats: beatsGroupedByGenres[genre],
-        } as BeatsGroupedByGenre));
-      },
-      catch: (error) => {
-        console.error("Error fetching beats:", error);
-        throw new Error("Can't get beats grouped by genre");
-      }
-    });
-
-    return Effect.runPromise(effect);
+    return firstValueFrom(of(beatsGroupedByGenre));
   };
-
-
-  private groupBy<T>(array: readonly T[], keyGetter: (item: T) => string): Record<string, T[]> {
-    return array.reduce((result, item) => {
-      const key = keyGetter(item);
-      if (!result[key]) {
-        result[key] = [];
-      }
-      result[key].push(item);
-      return result;
-    }, {} as Record<string, T[]>);
-  }
 }
 
