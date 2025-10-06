81c512b4ebb9aa890a8b62d7cc84d87a7f39208e
Baptiste <bab07ali@gmail.com>
2025-10-06 18:16:02 +0200

    Reformat karmaconfjs
    Enable code coverage
    
    add coverage result to CI
    
    add codecov coverage result
    
    add codecov coverage result 2
    
    Add coverage to test command

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -1,46 +1,42 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
+// karma.conf.js
 module.exports = function (config) {
-    config.set({
-        basePath: '',
-        frameworks: ['jasmine', '@angular-devkit/build-angular'],
-        plugins: [
-            require('karma-jasmine'),
-            require('karma-chrome-launcher'),
-            require('karma-jasmine-html-reporter'),
-            require('karma-coverage'),
-
-        ],
-        client: {
-            jasmine: {
-                // you can add configuration options for Jasmine here
-                // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-                // for example, you can disable the random execution with `random: false`
-                // or set a specific seed with `seed: 4321`
-            },
-            clearContext: false // leave Jasmine Spec Runner output visible in browser
-        },
-        jasmineHtmlReporter: {
-            suppressAll: true // removes the duplicated traces
-        },
-        coverageReporter: {
-            dir: require('path').join(__dirname, './coverage/ng17-lib-test'),
-            subdir: '.',
-            reporters: [
-                { type: 'html' },
-                { type: 'text-summary' }
-            ],
-            check: {
-                global: {
-                    statements: 100,
-                }
-            }
-        },
-        reporters: ['progress', 'kjhtml'],
-        browsers: ['ChromeHeadless'],
-        restartOnFileChange: true,
-        autoWatch: true,
-        singleRun: false,
-    });
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage')
+    ],
+    client: {
+      jasmine: {},
+      clearContext: false
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/ng17-lib-test'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' },
+        { type: 'lcovonly' }
+      ]
+    },
+    check: {
+      global: {
+        statements: 80,
+        branches: 80,
+        functions: 80,
+        lines: 80
+      }
+    },
+    reporters: ['progress', 'kjhtml', 'coverage'],
+    browsers: ['ChromeHeadless'],
+    restartOnFileChange: true,
+    autoWatch: true,
+    singleRun: false
+  });
 };
