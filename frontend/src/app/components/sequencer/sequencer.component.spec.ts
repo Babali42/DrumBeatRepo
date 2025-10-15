fa038cb57e103352c1c3168b725e918e6c28eb93
Baptiste Lyet <bab07ali@gmail.com>
2025-10-15 23:24:32 +0200

    (non) Feature : delete tap tempo component

--- frontend/src/app/components/sequencer/sequencer.component.spec.ts
+++ frontend/src/app/components/sequencer/sequencer.component.spec.ts
@@ -1,5 +1,5 @@
 import {SequencerComponent} from "./sequencer.component";
-import {ComponentFixture, fakeAsync, TestBed, tick} from "@angular/core/testing";
+import {ComponentFixture, TestBed} from "@angular/core/testing";
 import {InMemoryBeatGateway} from "../../adapters/secondary/in-memory-beat-gateway";
 import {provideRouter} from "@angular/router";
 import {provideHttpClient} from "@angular/common/http";
@@ -40,42 +40,6 @@ describe('SequencerComponent', () => {
   it('should create', () => {
     expect(component).toBeDefined();
   });
-
-  it('should have <button> with "Tap tempo"', () => {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const sequencerComponent: HTMLElement = fixture.nativeElement;
-    const buttons = sequencerComponent.querySelectorAll('button');
-    const p = buttons[buttons.length - 1];
-    expect(p.textContent).toEqual('Tap tempo');
-  });
-
-  it('should update tempo after three tap tempo clicks', fakeAsync(() => {
-    //Arrange
-    fixture.detectChanges();
-
-    // eslint-disable-next-line
-    const sequencerComponent: HTMLElement = fixture.nativeElement;
-    const buttons = sequencerComponent.querySelectorAll('button');
-    const tapTempoButton = buttons[1];
-
-    expect(tapTempoButton).toBeTruthy();
-
-    //Act
-    tapTempoButton.click();
-    tick(300);
-    tapTempoButton.click();
-    tick(300);
-    tapTempoButton.click();
-
-    fixture.detectChanges();
-
-    //Assert
-    // eslint-disable-next-line
-    const tempoInput = fixture.nativeElement.querySelector('input');
-    // eslint-disable-next-line
-    const tempoResult = parseInt(tempoInput.value, 10);
-    expect(tempoResult).toBe(200);
-  }))
 
   it("should display a step button", () => {
     fixture.detectChanges();
@@ -140,5 +104,3 @@ describe('SequencerComponent', () => {
     });
   });
 })
-
-
