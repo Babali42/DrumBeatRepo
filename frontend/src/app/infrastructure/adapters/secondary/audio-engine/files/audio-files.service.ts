6305c743225c83cd28873fb01117ad6233985cfa
Baptiste Lyet <bab07ali@gmail.com>
2026-02-03 17:59:56 +0100

    BugFix : Non existing sound file link should be handled with no error in console

--- frontend/src/app/infrastructure/adapters/secondary/audio-engine/files/audio-files.service.ts
+++ frontend/src/app/infrastructure/adapters/secondary/audio-engine/files/audio-files.service.ts
@@ -1,10 +1,19 @@
+import {Option} from "effect";
+
 export class AudioFilesService {
-  async getAudioBuffer(soundName: string): Promise<AudioBuffer> {
-    const myRequest = new Request(`assets/sounds/${soundName}`);
-    const response = await fetch(myRequest);
-    const arrayBuffer = await response.arrayBuffer();
-    return await new AudioContext().decodeAudioData(arrayBuffer).then((data) => {
-      return data
-    });
+  async getAudioBuffer(soundName: string): Promise<Option.Option<AudioBuffer>> {
+    try {
+      const response = await fetch(`assets/sounds/${soundName}`);
+
+      if (!response.ok)
+        return Option.none();
+
+      const arrayBuffer = await response.arrayBuffer();
+      const audioBuffer = await new AudioContext().decodeAudioData(arrayBuffer);
+
+      return Option.some(audioBuffer);
+    } catch {
+      return Option.none();
+    }
   }
 }
