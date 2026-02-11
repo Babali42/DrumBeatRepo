cc42d764fbfab5ba2d4301c8ccf0bab0676f9032
Baptiste Lyet <bab07ali@gmail.com>
2026-02-11 21:11:31 +0100

    Feature : translate app

--- frontend/src/app/ui/app.module.ts
+++ frontend/src/app/ui/app.module.ts
@@ -15,7 +15,7 @@ import {AudioEngineAdapter} from "../infrastructure/adapters/secondary/audio-eng
 import {JsonFileReader} from "../infrastructure/adapters/secondary/beat-source/json-files-reader.service";
 import {IManageBeatsToken} from "../infrastructure/injection-tokens/i-manage-beat.token";
 import {jsonFileReaderToken} from "../infrastructure/injection-tokens/json-file-reader.token";
-import {provideTranslateService} from "@ngx-translate/core";
+import {provideTranslateService, TranslatePipe} from "@ngx-translate/core";
 import {provideTranslateHttpLoader} from "@ngx-translate/http-loader";
 
 export const routes: Routes = [
@@ -32,7 +32,7 @@ RouterModule.forRoot(routes, {
   imports: [BrowserModule,
     LoadingBarModule,
     FormsModule,
-    RouterOutlet
+    RouterOutlet, TranslatePipe
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true},
