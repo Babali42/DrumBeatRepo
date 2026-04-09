1d02c7eebdcb7c7f813e9f00cfae318bf91d3d57
Baptiste Lyet <bab07ali@gmail.com>
2026-04-09 15:05:48 +0200

    Refacto : Use optional field for midiType

--- frontend/src/app/core/testing/track.spec.vite.ts
+++ frontend/src/app/core/testing/track.spec.vite.ts
@@ -1,8 +1,9 @@
 import {Track} from "../domain/track";
-import { expect, test } from 'vitest'
+import {expect, test} from 'vitest'
+import {MidiDrumType} from "../domain/midi-drum-type";
 
 test("Should not be created with unsupported step count", () => {
-  expect(() => new Track("Kick", "", [true, false])).toThrow();
+  expect(() => new Track("Kick", "", [true, false], MidiDrumType.BASS_DRUM_1)).toThrow();
 });
 
 test("Should be created with right step number", () => {
