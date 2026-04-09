46c403208240030e2f463adf2355665f76108324
Baptiste Lyet <bab07ali@gmail.com>
2026-04-09 15:22:55 +0200

    Tests : Correct the tests after the optional refacto

--- frontend/src/app/core/testing/track.spec.vite.ts
+++ frontend/src/app/core/testing/track.spec.vite.ts
@@ -1,12 +1,13 @@
 import {Track} from "../domain/track";
 import {expect, test} from 'vitest'
 import {MidiDrumType} from "../domain/midi-drum-type";
+import {Option} from "effect";
 
 test("Should not be created with unsupported step count", () => {
-  expect(() => new Track("Kick", "", [true, false], MidiDrumType.BASS_DRUM_1)).toThrow();
+  expect(() => new Track("Kick", "", [true, false], Option.some(MidiDrumType.BASS_DRUM_1))).toThrow();
 });
 
 test("Should be created with right step number", () => {
-  const track = new Track("Kick", "", [true, false, false, false, true, false, false, false, true, false, false, false, true, false, false, false]);
+  const track = new Track("Kick", "", [true, false, false, false, true, false, false, false, true, false, false, false, true, false, false, false], Option.some(MidiDrumType.BASS_DRUM_1));
   expect(track).toBeDefined();
 });
