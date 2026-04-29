7289290ef87918e58accc0ac0d80cf899060ac61
Baptiste Lyet <bab07ali@gmail.com>
2026-04-29 18:17:50 +0200

    chore : change bpm input component to use reactive form

--- frontend/src/app/ui/components/bpm-input/bpm-input.component.spec.ts
+++ frontend/src/app/ui/components/bpm-input/bpm-input.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { BpmInputComponent } from './bpm-input.component';
 import {By} from "@angular/platform-browser";
+import {ReactiveFormsModule} from "@angular/forms";
 
 type BeatChangeDataSet = {
   min: number;
@@ -16,9 +16,8 @@ describe('BpmInputComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [BpmInputComponent]
-    })
-    .compileComponents();
+      imports: [ReactiveFormsModule]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(BpmInputComponent);
     component = fixture.componentInstance;
@@ -41,12 +40,9 @@ describe('BpmInputComponent', () => {
       fixture.componentRef.setInput('bpm', actual);
       fixture.detectChanges();
 
-      let lastValue!: number;
-      component.bpmChange.subscribe(v => lastValue = v);
-
       component.incrementBpm();
 
-      expect(lastValue).toBe(expected);
+      expect(component.form.controls.bpm.value).toBe(expected);
     });
   });
 
@@ -62,12 +58,9 @@ describe('BpmInputComponent', () => {
       fixture.componentRef.setInput('bpm', actual);
       fixture.detectChanges();
 
-      let lastValue!: number;
-      component.bpmChange.subscribe(v => lastValue = v);
-
       component.decrementBpm();
 
-      expect(lastValue).toBe(expected);
+      expect(component.form.controls.bpm.value).toBe(expected);
     });
   });
 
@@ -83,8 +76,28 @@ describe('BpmInputComponent', () => {
     ).nativeElement as HTMLInputElement;
 
     input.value = '120';
-    input.dispatchEvent(new Event('change'));
+    input.dispatchEvent(new Event('input'));
 
     expect(lastValue).toBe(120);
+  });
+
+  it('should be valid when the field is filled in correctly', () => {
+    component.form.controls.bpm.setValue(120);
+    expect(component.form.valid).toBe(true);
+  });
+
+  it('should not be valid when the field is empty', () => {
+    component.form.controls.bpm.setValue(null);
+    expect(component.form.valid).toBe(false);
+  });
+
+  it('should not be valid when the bpm is out of bounds (min)', () => {
+    component.form.controls.bpm.setValue(1);
+    expect(component.form.valid).toBe(false);
+  });
+
+  it('should not be valid when the bpm is out of bounds (max)', () => {
+    component.form.controls.bpm.setValue(100000);
+    expect(component.form.valid).toBe(false);
   });
 });
