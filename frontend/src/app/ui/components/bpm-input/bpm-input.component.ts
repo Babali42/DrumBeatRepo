65c507bc320c01e990cbe41fab3ac10377b6bd2f
Baptiste Lyet <bab07ali@gmail.com>
2026-05-06 11:06:55 +0200

    refacto (bpm-input) : use new input instead of @Input

--- frontend/src/app/ui/components/bpm-input/bpm-input.component.ts
+++ frontend/src/app/ui/components/bpm-input/bpm-input.component.ts
@@ -1,28 +1,26 @@
-import {Component, inject, Input, OnChanges, output, SimpleChanges} from '@angular/core';
-import {LongPressDirective} from "../../directives/long-press.directive";
-import {maxBpm, minBpm} from "../../../domain/bpm";
-import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
-import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
-import {distinctUntilChanged, filter, map} from "rxjs/operators";
+import { Component, effect, inject, input, output } from '@angular/core';
+import { LongPressDirective } from "../../directives/long-press.directive";
+import { maxBpm, minBpm } from "../../../domain/bpm";
+import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
+import { distinctUntilChanged, filter, map } from "rxjs/operators";
 
 @Component({
   selector: 'app-bpm-input',
   templateUrl: './bpm-input.component.html',
-  imports: [
-    LongPressDirective,
-    ReactiveFormsModule
-  ],
+  imports: [LongPressDirective, ReactiveFormsModule],
   styleUrl: './bpm-input.component.scss'
 })
-export class BpmInputComponent implements OnChanges {
+export class BpmInputComponent {
   private readonly fb = inject(FormBuilder);
+
   maxBpm = maxBpm;
   minBpm = minBpm;
   bpmChange = output<number>();
+  bpm = input(128);
 
-  @Input() bpm!: number;
   form = this.fb.group({
-    bpm: [this.bpm, [
+    bpm: [this.bpm(), [
       Validators.required,
       Validators.min(minBpm),
       Validators.max(maxBpm)
@@ -30,6 +28,10 @@ export class BpmInputComponent implements OnChanges {
   });
 
   constructor() {
+    effect(() => {
+      this.updateBpm(this.bpm(), false);  // runs on init + every change
+    });
+
     this.form.controls.bpm.valueChanges
       .pipe(
         filter(() => this.form.controls.bpm.valid),
@@ -41,13 +43,8 @@ export class BpmInputComponent implements OnChanges {
   }
 
   incrementBpm = () => this.updateBpm(Math.min(this.form.controls.bpm.value! + 1, this.maxBpm), true);
+
   decrementBpm = () => this.updateBpm(Math.max(this.form.controls.bpm.value! - 1, this.minBpm), true);
 
   private readonly updateBpm = (value: number, emitEvent: boolean) => this.form.controls.bpm.setValue(Number(value), { emitEvent: emitEvent });
-
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes['bpm']) {
-      this.updateBpm(changes['bpm'].currentValue, false);
-    }
-  }
 }
