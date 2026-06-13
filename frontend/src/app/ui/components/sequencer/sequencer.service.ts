bf11edd2b4f0b250651b9ae31060e44e692d8b84
Babali42 <ba07ali@gmail.com>
2026-06-13 17:19:52 +0200

    wip

---
+++ frontend/src/app/ui/components/sequencer/sequencer.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+export interface Command {
+  type: string;
+  payload?: any;
+}
+
+@Injectable()
+export class SequencerService {
+
+  state$ = new BehaviorSubject<any>(null);
+
+  dispatch(cmd: Command) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
+    SequencerEngine.dispatch(cmd);
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
+    this.state$.next(SequencerEngine.getState());
+  }
+}
