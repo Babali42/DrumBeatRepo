3cc680878b70da57a7c014592c955ccb9c4b578a
Baptiste Lyet <bab07ali@gmail.com>
2026-06-14 12:13:27 +0200

    Undo redo buttons (accessibility) : fix coderabbits comments

--- frontend/src/app/ui/components/sequencer/sequencer.component.ts
+++ frontend/src/app/ui/components/sequencer/sequencer.component.ts
@@ -57,7 +57,10 @@ export class SequencerComponent implements OnInit, OnDestroy {
   selectedGenreLabel: string = "";
   isAudioExportModalOpen = false;
   isMidiExportModalOpen = false;
+  //TODO : will be implemented with the command pattern
   historyLength: number = 0;
+
+  //TODO : will be implemented with the command pattern
   futureLength: number = 0;
 
   constructor(@Inject(IManageBeatsToken) private readonly _beatsManager: IManageBeats,
