a08f095aa793e82f243dde99a80199fd817f91bd
Baptiste Lyet <bab07ali@gmail.com>
2026-07-09 20:13:24 +0200

    bugfix : empty future collections when doing an action

--- engine/src/main/scala/com/drumbeatrepo/sequencer/SequencerState.scala
+++ engine/src/main/scala/com/drumbeatrepo/sequencer/SequencerState.scala
@@ -7,16 +7,15 @@ case class SequencerState(
     history: List[SequencerState],
     future: List[SequencerState]
 ):
-
   def dispatch(command: Command): SequencerState = command match
     case Command.SelectBeat(newGenre, newBeat, newTempo) =>
-      SequencerState(newGenre, newBeat, newTempo, history :+ this, future)
+      SequencerState(newGenre, newBeat, newTempo, history :+ this, future = Nil)
     case Command.SetTempo(newTempo) =>
       history match
         case _ :+ last if last.genre == genre && last.beat == beat =>
-          SequencerState(genre, beat, newTempo, history, future)
+          SequencerState(genre, beat, newTempo, history, future = Nil)
         case _ =>
-          SequencerState(genre, beat, newTempo, history :+ this, future)
+          SequencerState(genre, beat, newTempo, history :+ this, future = Nil)
     case Command.Undo =>
       history match
         case init :+ last => last.copy(history = init, future = this :: future)
