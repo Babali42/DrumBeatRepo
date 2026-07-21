d31e02b98a64211eab901207f9f31526226667c8
Baptiste Lyet <bab07ali@gmail.com>
2026-07-21 23:08:37 +0200

    #499 - translate midi drum type

---
+++ frontend/src/app/ui/services/mididrumtype-to-text.service.ts
@@ -0,0 +1,15 @@
+import { inject, Injectable } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
+import { firstValueFrom } from "rxjs";
+import { MidiDrumType } from "src/app/domain/midi-drum-type";
+
+@Injectable({ providedIn: 'root' })
+export class MidiDrumTypeToTextService {
+    private translateService = inject(TranslateService);
+
+    async getMidiDrumTypeText(value: MidiDrumType): Promise<string> {
+        return firstValueFrom(
+            this.translateService.get(`MidiDrumType.${value}`)
+        );
+    }
+}
