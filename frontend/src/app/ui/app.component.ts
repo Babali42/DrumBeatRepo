c40e64ef2119576a64112d452493ab87ab005d47
Baptiste Lyet <44911483+Babali42@users.noreply.github.com>
2026-09-01 10:59:16 +0000

    remove router

--- frontend/src/app/ui/app.component.ts
+++ frontend/src/app/ui/app.component.ts
@@ -2,18 +2,18 @@ import {Component, HostListener} from '@angular/core';
 import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
 import {ModeToggleService} from "./services/light-dark-mode/mode-toggle.service";
 import {Mode} from './services/light-dark-mode/mode-toggle.model';
-import {Router, RouterOutlet} from "@angular/router";
 import {toSignal} from "@angular/core/rxjs-interop";
 import {map} from "rxjs/operators";
 import {TranslatePipe} from "@ngx-translate/core";
 import {LoadingBarModule} from "@ngx-loading-bar/core";
+import {SequencerComponent} from './components/sequencer/sequencer.component';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
     standalone: true,
-    imports: [RouterOutlet, TranslatePipe, LoadingBarModule]
+    imports: [SequencerComponent, TranslatePipe, LoadingBarModule]
 })
 export class AppComponent {
   isPortrait: boolean = false;
@@ -28,8 +28,7 @@ export class AppComponent {
   );
 
   constructor(private readonly responsive: BreakpointObserver,
-              private readonly modeToggleService: ModeToggleService,
-              private readonly router: Router) {
+              private readonly modeToggleService: ModeToggleService) {
     this.modeToggleService.modeChanged$.subscribe(x => this.mode = x);
     this.checkOrientation();
   }
@@ -47,12 +46,7 @@ export class AppComponent {
 
   protected readonly Mode = Mode;
 
-  async goToMainPage() {
-    await this.router.navigate([], {
-      queryParams: {},
-      queryParamsHandling: '',
-    }).then(() => {
-      window.location.reload();
-    }).catch();
+  goToMainPage() {
+     window.location.reload();
   }
 }
